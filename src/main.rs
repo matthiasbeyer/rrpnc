4f7233da3d9b512063fbf53173ef533664790529
Matthias Beyer <mail@beyermatthias.de>
2017-11-15 12:17:42 +0100

    Switch to LinkedList for Stack

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use std::collections::LinkedList as LL;
 fn main() {
-    let mut stack = vec![];
+    let mut stack = LL::new();
 
-    fn operator<F: FnOnce(&mut Vec<f64>, f64, f64) -> ()>(stack: &mut Vec<f64>, op: F) {
-        match (stack.pop(), stack.pop()) {
+    fn operator<F: FnOnce(&mut LL<f64>, f64, f64) -> ()>(stack: &mut LL<f64>, op: F) {
+        match (stack.pop_back(), stack.pop_back()) {
             (Some(l), Some(r)) => op(stack, l, r),
             (None, _) => println!("No left operant"),
             (Some(a), None) => {
-                stack.push(a);
+                stack.push_back(a);
                 println!("No right operant")
             },
         }
@@ -14,25 +15,25 @@ fn main() {
 
     for arg in ::std::env::args().skip(1) {
         match arg.as_ref() {
-            "+" => operator(&mut stack, |s, l, r| s.push(r + l)),
-            "-" => operator(&mut stack, |s, l, r| s.push(r - l)),
-            "*" => operator(&mut stack, |s, l, r| s.push(r * l)),
+            "+" => operator(&mut stack, |s, l, r| s.push_back(r + l)),
+            "-" => operator(&mut stack, |s, l, r| s.push_back(r - l)),
+            "*" => operator(&mut stack, |s, l, r| s.push_back(r * l)),
             "/" => operator(&mut stack, |s, l, r| if l == 0.0 {
-                s.push(l);
-                s.push(r);
+                s.push_back(l);
+                s.push_back(r);
                 println!("Cannot divide by zero");
             } else {
-                s.push(r / l);
+                s.push_back(r / l);
             }),
 
-            "p" => if let Some(f) = stack.pop() {
+            "p" => if let Some(f) = stack.pop_back() {
                 println!("{}", f);
             },
 
             "q" => ::std::process::exit(1),
 
             _ => if let Ok(s) = ::std::str::FromStr::from_str(&arg) {
-                stack.push(s);
+                stack.push_back(s);
             } else {
                 println!("Can not parse: '{}'", arg);
             },
